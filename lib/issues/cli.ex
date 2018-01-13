645e7b4811b5348c5bff0b303a0df1efd3c5f32a
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-01-13 20:42:42 +0800

    Add formatting

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,6 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2 ]
+
   @default_count 4
 
   @moduledoc """
@@ -6,7 +8,7 @@ defmodule Issues.CLI do
   that end up generating a table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -50,6 +52,9 @@ defmodule Issues.CLI do
   def process({ user, project, count }) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response
+    |> sort_into_ascending_order
+    |> Enum.take(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({ :ok, body }), do: body
@@ -58,6 +63,11 @@ defmodule Issues.CLI do
     { _, message } = List.keyfind(error, "message", 0)
     IO.puts "Error fetching from Github: #{message}"
     System.halt(2)
+  end
+
+  def sort_into_ascending_order(list_of_issues) do
+    Enum.sort list_of_issues, fn i1, i2 -> 
+      Map.get(i1, "created_at") <= Map.get(i2, "created_at") end
   end
 
 end
