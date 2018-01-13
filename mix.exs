88e87bed6e60f289ba2e0d144b12121e0e0e2c67
Faiz Mokhtar <mfmokhtar@gmail.com>
2018-01-13 21:34:06 +0800

    Add coveralls

--- mix.exs
+++ mix.exs
@@ -5,12 +5,19 @@ defmodule Issues.Mixfile do
     [
       app: :issues,
       escript: escript_config(),
+      name: "Issues",
+      source_url: "https://github.com/faizmokhtar/ToyElixirIssues",
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+      ],
       deps: deps(),
-      name: "Issues",
-      source_url: "https://github.com/faizmokhtar/ToyElixirIssues"
     ]
   end
 
@@ -28,6 +35,7 @@ defmodule Issues.Mixfile do
       {:poison, "~> 2.2"},
       {:ex_doc, "~> 0.12"},
       {:earmark, "~> 1.0", override: true},
+      {:excoveralls, "~> 0.5", only: :test},
     ]
   end
 
